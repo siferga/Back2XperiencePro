f06f4a47f10229c2b92847b95a389daefdd1c944
Siferga <sifergadeveloper@gmail.com>
2024-06-29 22:35:15 +0200

    eurekaserver ms

--- eurekaserver-b2x/src/main/java/com/siferga/eurekaserver_b2x/EurekaserverB2xApplication.java
+++ eurekaserver-b2x/src/main/java/com/siferga/eurekaserver_b2x/EurekaserverB2xApplication.java
@@ -2,8 +2,10 @@ package com.siferga.eurekaserver_b2x;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class EurekaserverB2xApplication {
 
 	public static void main(String[] args) {

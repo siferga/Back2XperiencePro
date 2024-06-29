e42d588cb89162d2dc1b0dd0dbacbcba9d7784aa
Siferga <sifergadeveloper@gmail.com>
2024-06-29 22:33:47 +0200

    configserver

--- configserver-b2x/src/main/java/com/siferga/configserver_b2x/ConfigserverB2xApplication.java
+++ configserver-b2x/src/main/java/com/siferga/configserver_b2x/ConfigserverB2xApplication.java
@@ -2,8 +2,11 @@ package com.siferga.configserver_b2x;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
 
 @SpringBootApplication
+@EnableConfigServer
 public class ConfigserverB2xApplication {
 
 	public static void main(String[] args) {

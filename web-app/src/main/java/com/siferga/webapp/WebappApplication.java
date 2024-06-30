3f854a92eee5dc05e0320c6bf8bada5532231745
Siferga <sifergadeveloper@gmail.com>
2024-06-30 21:40:03 +0200

    webapp microservice

---
+++ web-app/src/main/java/com/siferga/webapp/WebappApplication.java
@@ -0,0 +1,15 @@
+package com.siferga.webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients
+public class WebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebappApplication.class, args);
+	}
+
+}

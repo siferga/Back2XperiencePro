9fa0ac39d5ae16cfe65d0be661229ff322b167e9
siferga <silvia.fernandez.pro@gmail.com>
2024-09-25 10:53:54 +0200

    Project model for wabapp creation

---
+++ webapp/src/main/java/com/siferga/webapp/model/Project.java
@@ -0,0 +1,25 @@
+package com.siferga.webapp.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "projects")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Project {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String description;
+
+
+}
+

eb5eef267a7643f3ed18619c05380dde0d78c9b4
siferga <silvia.fernandez.pro@gmail.com>
2024-09-26 15:12:24 +0200

    Project model creation in project ms

---
+++ project-b2x/src/main/java/com/siferga/project/model/Project.java
@@ -0,0 +1,25 @@
+package com.siferga.project.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Data
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class Project {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+    private String client;
+
+
+}

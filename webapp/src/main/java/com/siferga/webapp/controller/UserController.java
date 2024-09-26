b5d60275bbfeacf0db4299fbeeb914f187b6bb7f
siferga <silvia.fernandez.pro@gmail.com>
2024-09-26 15:11:22 +0200

    moving from webapp Usercontroller to AdminController the sign up

--- webapp/src/main/java/com/siferga/webapp/controller/UserController.java
+++ webapp/src/main/java/com/siferga/webapp/controller/UserController.java
@@ -1,11 +1,12 @@
 package com.siferga.webapp.controller;
 
-
 import ch.qos.logback.core.model.Model;
 import com.siferga.webapp.service.CollaboratorServiceImpl;
 import com.siferga.webapp.service.UserServiceImpl;
 import lombok.RequiredArgsConstructor;
 import com.siferga.webapp.model.User;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -19,9 +20,22 @@ public class UserController
     private final UserServiceImpl userServiceImpl;
     private final CollaboratorServiceImpl collaboratorServiceImpl;
 
+//    @GetMapping("/")
+//    public ModelAndView home() {
+//        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+//        if (authentication != null && authentication.isAuthenticated() && !(authentication.getPrincipal() instanceof String)) {
+//            // L'utilisateur est authentifié
+//            return new ModelAndView("dashboardCollaborator");
+//        } else {
+//            // L'utilisateur n'est pas authentifié, redirection vers la page de login
+//            return new ModelAndView("redirect:/login");
+//        }
+//    }
+
+
     @GetMapping("/")
     public ModelAndView home(Model model) {
-        return new ModelAndView("index");
+        return new ModelAndView("dashboardCollaborator");
     }
     @GetMapping("/login")
     public ModelAndView getLoginPage() {
@@ -45,12 +59,5 @@ public class UserController
     public ModelAndView showRegisterForm() {
         return new ModelAndView("/signup","user",new User());
     }
-
-//    @PostMapping("/addPatient")
-//    public ModelAndView addPatientForm(@ModelAttribute Patient patient) {
-//        patientServiceImpl.registerPatient(patient);
-//        return new ModelAndView("/patientList","patient",new Patient());
-//    }
-
 
 }

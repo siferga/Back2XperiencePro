703df8fe245e13d0d8b34fa271905a6a785c1949
siferga <silvia.fernandez.pro@gmail.com>
2024-09-26 15:08:40 +0200

    User class in webapp

--- webapp/src/main/java/com/siferga/webapp/model/User.java
+++ webapp/src/main/java/com/siferga/webapp/model/User.java
@@ -1,9 +1,6 @@
 package com.siferga.webapp.model;
+import com.netflix.eventbus.spi.Subscribe;
 import lombok.*;
-
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
@@ -12,8 +9,46 @@ public class User {
     private Long id;
     private String email;
     private String password;
+
     // Example of Role enum, you can customize it as per your requirement
-    public enum Role {
-        ADMIN, USER, GUEST
-    }
+//    public enum Role {
+//       ADMIN, USER, GUEST
+//    }
 }
+
+
+
+//package com.siferga.webapp.model;
+
+//import jakarta.persistence.*;
+//import lombok.*;
+//
+//import java.util.HashSet;
+//import java.util.Set;
+
+//@Entity
+//@Data
+//@AllArgsConstructor
+//@NoArgsConstructor
+//public class User {
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private Long id;
+//
+//    @Column(unique = true, nullable = false)
+//    private String email;
+//
+//   @Column(nullable = false)
+//    private String password;
+//
+//    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+//    @JoinTable(
+//            name = "user_roles",
+//            joinColumns = @JoinColumn(name = "user_id"),
+//            inverseJoinColumns = @JoinColumn(name = "role_id")
+//    )
+//    private Set<Role> roles;
+//}
+
+
+
